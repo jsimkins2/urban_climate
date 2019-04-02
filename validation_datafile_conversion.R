92b6ff5b038fba0e90c3db466e6a84537c1cd4bd
jsimkins2 <jamessimkins5@gmail.com>
2019-04-02 10:41:20 -0400

    edited validation checks2

--- validation_datafile_conversion.R
+++ validation_datafile_conversion.R
@@ -1,5 +1,6 @@
 # data conversion validation
 # this r script is going to convert the arcgrid data to geotiff to netcdf to ascii
+# James Simkins
 
 # load required libraries
 library(raster)
@@ -7,25 +8,25 @@ library(raster)
 # going to pull together a list of dirnames and randomly select ones to check
 years = seq(2010, 2100, 10)
 ssp = paste0('ssp', seq(1,5), '_')
-
 dirnames = list()
 for (y in years){
   for (s in ssp){
     dirnames = append(dirnames, paste0(s, y))
   }
 }
-rowLen = seq(1,1117)
-colLen = seq(1,2880)
+
+# Begin Loop
 for (i in seq_len(150)){
   #randomly select a file, a row, and a column
   randSSP = sample(dirnames, 1)
   
-  # read in the data as raster
+  # read geotiff data in the data as raster and convert to matrix for checking
   temrast1 = raster(paste0("/Users/james/Documents/Delaware/urban_climate/datasets/1_8_degree_urban_extent_projections/UrbanExtentProjections_SSPs1-5_2010-2100_v1_GEOTIFF_1_8_dgr/SSP", substr(randSSP[[1]], 4, 4), "_GeoTIFF/",randSSP[[1]], ".tif"))
   ccrs = crs(temrast1)
   cextent = extent(temrast1)
   r1=as.matrix(temrast1)
   
+  # read netcdf data in the data as raster, add in projection, and convert to matrix for checking
   x = ncdf4::nc_open(paste0("/Users/james/Documents/Delaware/urban_climate/datasets/1_8_degree_urban_extent_projections/UrbanExtentProjections_SSPs1-5_2010-2100_v1_NETCDF_1_8_dgr/SSP", substr(randSSP[[1]], 4, 4), "_NETCDF/",randSSP[[1]], ".nc"))
   b1 = ncdf4::ncvar_get(x, "Band1")
   temrast2 = raster(b1)
@@ -34,9 +35,11 @@ for (i in seq_len(150)){
   extent(temrast2) = cextent
   r2 = as.matrix(temrast2)
   
+  # read ascii data in the data as rasterand convert to matrix for checking
   temrast3 = raster(paste0("/Users/james/Documents/Delaware/urban_climate/datasets/1_8_degree_urban_extent_projections/UrbanExtentProjections_SSPs1-5_2010-2100_v1_ASCII_1_8_dgr/SSP", substr(randSSP[[1]], 4, 4), "_ASCII/",randSSP[[1]], ".asc"))
   r3 = as.matrix(temrast3)
 
+  # perform all equal check to make sure that matrices are identical
   if (all.equal(r1,r2) == TRUE){
     if (all.equal(r1,r3) == TRUE){
       print("All Equal!")
