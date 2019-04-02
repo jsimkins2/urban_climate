38da4a99dbc8d25b5d5414c56de59dedd7da47cf
jsimkins2 <jamessimkins5@gmail.com>
2019-04-02 10:37:22 -0400

    edited validation checks

--- validation_datafile_conversion.R
+++ validation_datafile_conversion.R
@@ -19,35 +19,30 @@ colLen = seq(1,2880)
 for (i in seq_len(150)){
   #randomly select a file, a row, and a column
   randSSP = sample(dirnames, 1)
-  randRow = sample(rowLen, 1)
-  randCol = sample(colLen, 1)
   
   # read in the data as raster
-  temrast = raster(paste0("/Users/james/Documents/Delaware/urban_climate/datasets/1_8_degree_urban_extent_projections/UrbanExtentProjections_SSPs1-5_2010-2100_v1_GEOTIFF_1_8_dgr/SSP", substr(randSSP[[1]], 4, 4), "_GeoTIFF/",randSSP[[1]], ".tif"))
-  tifVal = getValues(temrast, randRow)[randCol]
-  ccrs = crs(temrast)
-  cextent = extent(temrast)
+  temrast1 = raster(paste0("/Users/james/Documents/Delaware/urban_climate/datasets/1_8_degree_urban_extent_projections/UrbanExtentProjections_SSPs1-5_2010-2100_v1_GEOTIFF_1_8_dgr/SSP", substr(randSSP[[1]], 4, 4), "_GeoTIFF/",randSSP[[1]], ".tif"))
+  ccrs = crs(temrast1)
+  cextent = extent(temrast1)
+  r1=as.matrix(temrast1)
   
   x = ncdf4::nc_open(paste0("/Users/james/Documents/Delaware/urban_climate/datasets/1_8_degree_urban_extent_projections/UrbanExtentProjections_SSPs1-5_2010-2100_v1_NETCDF_1_8_dgr/SSP", substr(randSSP[[1]], 4, 4), "_NETCDF/",randSSP[[1]], ".nc"))
   b1 = ncdf4::ncvar_get(x, "Band1")
-  temrast = raster(b1)
-  temrast = t(temrast)
-  crs(temrast) = as.character(ccrs)
-  extent(temrast) = cextent
-  ncVal = getValues(temrast, randRow)[randCol]
+  temrast2 = raster(b1)
+  temrast2 = t(temrast2)
+  crs(temrast2) = as.character(ccrs)
+  extent(temrast2) = cextent
+  r2 = as.matrix(temrast2)
   
-  temrast = raster(paste0("/Users/james/Documents/Delaware/urban_climate/datasets/1_8_degree_urban_extent_projections/UrbanExtentProjections_SSPs1-5_2010-2100_v1_ASCII_1_8_dgr/SSP", substr(randSSP[[1]], 4, 4), "_ASCII/",randSSP[[1]], ".asc"))
-  crs(temrast) = as.character(ccrs)
-  extent(temrast) = cextent
-  asciiVal = getValues(temrast, randRow)[randCol]
-  
-  # print a flag if the geotiff values and nc values aren't equal
-  # note that the ascii files are all wrong here
-  if (is.na(ncVal) == FALSE){
-    if (ncVal != tifVal | ncVal != asciiVal){
-        print(paste0("NOT EQUAL ", "ncVal=",ncVal, " tifVal=",tifVal, " asciiVal=", asciiVal," row", randRow, " col", randCol, " file",randSSP[[1]]))
+  temrast3 = raster(paste0("/Users/james/Documents/Delaware/urban_climate/datasets/1_8_degree_urban_extent_projections/UrbanExtentProjections_SSPs1-5_2010-2100_v1_ASCII_1_8_dgr/SSP", substr(randSSP[[1]], 4, 4), "_ASCII/",randSSP[[1]], ".asc"))
+  r3 = as.matrix(temrast3)
+
+  if (all.equal(r1,r2) == TRUE){
+    if (all.equal(r1,r3) == TRUE){
+      print("All Equal!")
     } else {
-      print(paste0("ALL EQUAL ", "ncVal=",ncVal, " tifVal=",tifVal, " asciiVal=", asciiVal))
+      print("OH NO THEY AREN'T EQUAL")
     }
   }
 }
+
