3524491416b716140744637c54542a9932951b0b
jsimkins2 <jamessimkins5@gmail.com>
2019-05-01 09:57:33 -0400

    asdf

--- BU_land1km.R
+++ BU_land1km.R
@@ -54,7 +54,7 @@ rep = raster("/Users/james/Documents/Delaware/urban_climate/datasets/TestData_20
 
 
 
-
+r = raster("/Users/james/Documents/Delaware/urban_climate/datasets/TestData_2000_GLOBE_38m/out_dem.tif")
 
 
 
@@ -64,9 +64,9 @@ rep = raster("/Users/james/Documents/Delaware/urban_climate/datasets/TestData_20
 #-scale 0 1
 
 
-#gdalwarp -t_srs '+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +no_defs' -tr 928 956 -r average GHS_BUILT_LDS2000_GLOBE_R2016A_3857_38_v1_0_p1.tif jing_p1.tif
+#gdalwarp -t_srs '+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +no_defs' -tr 1030.559 1030.586 -r average GHS_BUILT_LDS2000_GLOBE_R2016A_3857_38_v1_0_p1.tif jing_p1.tif
 
-#gdalwarp -t_srs '+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +no_defs' -tr 928 956 -r average GHS_BUILT_LDS2000_GLOBE_R2016A_3857_38_v1_0_p2.tif jing_p2.tif
+#gdalwarp -t_srs '+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +no_defs' -tr 1030.559 1030.586 -r average GHS_BUILT_LDS2000_GLOBE_R2016A_3857_38_v1_0_p2.tif jing_p2.tif
 
 
 #gdaltranslate -scale 0 1 
